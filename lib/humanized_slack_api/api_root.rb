566842879c2732d11dc40375c9a3b7ddc6f538f8
Kinoshita.Yasuhiro <WhoIsDissolvedGirl+github@gmail.com>
2015-06-23 23:48:35 +0900

    add token auth test

--- lib/humanized_slack_api/api_root.rb
+++ lib/humanized_slack_api/api_root.rb
@@ -3,8 +3,17 @@ module HumanizedSlackApi
     attr_reader :channels, :users
     def initialize(token)
       @slack = Slack::Client.new(token: token)
+      raise(AuthenticationError.new("Api auth faild for token '#{ApiRoot.mask_token(token)}'")) unless @slack.auth_test['ok']
       @channels = Channels.new(@slack.channels_list, self)
       @users = Users.new(@slack.users_list, self)
     end
+
+    def self.mask_token(token)
+      token_chunks = token.split('-')
+      last_chunk = token_chunks.pop
+      token_chunks.map { |chunk| 'X' * chunk.size }.push(last_chunk).join('-')
+    end
   end
+
+  class AuthenticationError < StandardError; end
 end
